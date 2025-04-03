0a83528823af1d435fbe83c4f57818efb7eda153
ryramzy <mramsay0@gmail.com>
2025-04-03 12:12:20 -0300

    Simplify PostCSS and Next.js configuration

--- next.config.js
+++ next.config.js
@@ -13,19 +13,6 @@ const nextConfig = {
     removeConsole: process.env.NODE_ENV === 'production',
   },
   poweredByHeader: false,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(woff|woff2|eot|ttf|otf)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: 'static/fonts/',
-        },
-      },
-    });
-    return config;
-  },
   headers: async () => {
     return [
       {
